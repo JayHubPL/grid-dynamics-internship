a3b9004093056312531544b488f07e19b33df419
Hubert Mazur <hmazur@griddynamics.com>
2022-08-22 13:20:38 +0200

    Used String.format for excepion message.

--- messages-split/src/main/java/com/griddynamics/WordTooLongException.java
+++ messages-split/src/main/java/com/griddynamics/WordTooLongException.java
@@ -5,7 +5,7 @@ public class WordTooLongException extends RuntimeException {
     private final String msg;
 
     WordTooLongException(String word, int limit) {
-        this.msg = "Word \"" + word + "\" is more than " + limit + " characters long!";
+        this.msg = String.format("Word \"%s\" is more than %d characters long!", word, limit);
     }
 
     @Override

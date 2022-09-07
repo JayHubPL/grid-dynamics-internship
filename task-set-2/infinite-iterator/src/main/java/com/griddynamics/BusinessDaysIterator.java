b5db8d8c387e1dc77f4c22a6384e949ea6c21cba
Hubert Mazur <hmazur@griddynamics.com>
2022-09-07 15:05:14 +0200

    Add JavaDoc and comments

--- task-set-2/infinite-iterator/src/main/java/com/griddynamics/BusinessDaysIterator.java
+++ task-set-2/infinite-iterator/src/main/java/com/griddynamics/BusinessDaysIterator.java
@@ -8,7 +8,12 @@ public class BusinessDaysIterator implements Iterator<LocalDate> {
 
     private LocalDate date;
 
-    // starting date is non-inclusive
+    /**
+     * Creates an infinite business day iterator, which skips Saturdays and Sundays,
+     * starting from {@code startingDate} excliusive.
+     * Iterator will finish if and only if the {@code LocalDate} format exceeds its maximum value.
+     * @param startingDate - a date to start iteration from, non-inclusive
+     */
     public BusinessDaysIterator(LocalDate startingDate) {
         if (startingDate == null) {
             throw new IllegalArgumentException("startingDate cannot be null");
@@ -21,6 +26,10 @@ public class BusinessDaysIterator implements Iterator<LocalDate> {
         try {
             getNextBusinessDay(date);
         } catch (DateTimeException dateTimeException) {
+            /* 
+             * This exception is thrown and caught if and only if the next
+             * business day exceeds LocalDate.MAX value.
+             */
             return false;
         }
         return true;
@@ -32,6 +41,12 @@ public class BusinessDaysIterator implements Iterator<LocalDate> {
         return date;
     }
 
+    /**
+     * Finds next business day after specified date. Skips Saturdays and Sundays.
+     * @param date - {@code LocalDate}, for which to find next business day
+     * @return next business day after {@code date}
+     * @throws DateTimeException when date exceeds {@code LocalDate.MAX}
+     */
     private LocalDate getNextBusinessDay(LocalDate date) throws DateTimeException {
         return switch (date.getDayOfWeek()) {
             case FRIDAY -> date.plusDays(3);

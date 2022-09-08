233a2058fe1083bfae6cefaf63dcd53c42045525
Hubert Mazur <hmazur@griddynamics.com>
2022-09-08 11:37:07 +0200

    Add new constructor and rename current iterator

--- task-set-2/flattening-iterator/src/main/java/com/griddynamics/FlatteningIterator.java
+++ task-set-2/flattening-iterator/src/main/java/com/griddynamics/FlatteningIterator.java
@@ -1,34 +1,46 @@
 package com.griddynamics;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
 
 public class FlatteningIterator<T> implements Iterator<T> {
 
     private final Iterator<Iterator<T>> iterators;
-    private Iterator<T> currentCollectionIterator;
+    private Iterator<T> currentIterator;
 
     @SafeVarargs
     public FlatteningIterator(Iterator<T>... iterators) {
         this.iterators = Arrays.asList(iterators).iterator();
+        currentIterator = Collections.emptyIterator();
         if (this.iterators.hasNext()) {
-            this.currentCollectionIterator = this.iterators.next();
-        } else {
-            throw new IllegalArgumentException("Not enough parameters provided, must be at least one");
+            currentIterator = this.iterators.next();
+        }
+    }
+
+    public FlatteningIterator(Collection<Iterator<T>> iterators) {
+        if (iterators == null) {
+            throw new IllegalArgumentException("Collection of iterators cannot be null");
+        }
+        this.iterators = iterators.iterator();
+        currentIterator = Collections.emptyIterator();
+        if (this.iterators.hasNext()) {
+            this.currentIterator = this.iterators.next();
         }
     }
 
     @Override
     public boolean hasNext() {
-        return currentCollectionIterator.hasNext() || iterators.hasNext();
+        return currentIterator.hasNext() || iterators.hasNext();
     }
 
     @Override
     public T next() {
-        while (!currentCollectionIterator.hasNext() && iterators.hasNext()) {
-            currentCollectionIterator = iterators.next();
+        while (!currentIterator.hasNext() && iterators.hasNext()) {
+            currentIterator = iterators.next();
         }
-        return currentCollectionIterator.next();        
+        return currentIterator.next();        
     }
     
 }

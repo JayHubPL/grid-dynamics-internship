ea37e4a6abc33b5243222d6e19c837e004d86005
Hubert Mazur <hmazur@griddynamics.com>
2022-09-22 13:36:06 +0200

    Remove field msg from exceptions

--- task-set-2/travel-agency/src/main/java/com/griddynamics/http/InvalidTokenException.java
+++ task-set-2/travel-agency/src/main/java/com/griddynamics/http/InvalidTokenException.java
@@ -2,15 +2,8 @@ package com.griddynamics.http;
 
 public class InvalidTokenException extends Exception {
 
-    private String msg;
-
     public InvalidTokenException(String token) {
-        msg = String.format("Authorization with token %s was not aquired", token);
-    }
-
-    @Override
-    public String getMessage() {
-        return msg;
+        super(String.format("Authorization with token %s was not aquired", token));
     }
 
 }
--- task-set-2/travel-agency/src/main/java/com/griddynamics/http/NoValidResponseException.java
+++ task-set-2/travel-agency/src/main/java/com/griddynamics/http/NoValidResponseException.java
@@ -3,16 +3,9 @@ package com.griddynamics.http;
 import com.griddynamics.flights.Flight;
 
 public class NoValidResponseException extends RuntimeException {
-
-    private final String msg;
-
+    
     public NoValidResponseException(Flight flight) {
-        msg = String.format("Failed to receive valid response for flight %s", flight);
-    }
-
-    @Override
-    public String getMessage() {
-        return msg;
+        super(String.format("Failed to receive valid response for flight %s", flight));
     }
     
 }

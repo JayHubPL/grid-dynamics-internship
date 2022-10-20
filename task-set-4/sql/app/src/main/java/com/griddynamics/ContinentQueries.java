72a52ebd36efaa8a58b9f40509c9b1f4bbcd7b33
Hubert Mazur <hmazur@griddynamics.com>
2022-10-20 21:48:56 +0200

    Add Continent related queries

--- task-set-4/sql/app/src/main/java/com/griddynamics/ContinentQueries.java
+++ task-set-4/sql/app/src/main/java/com/griddynamics/ContinentQueries.java
@@ -48,12 +48,33 @@ public class ContinentQueries {
     // 4. For each continent, find a country with the smallest area (print continent name, country name and area)
     public List<Triplet<String, String, Integer>> smallestCountryOnTheContinent() throws SQLException {
         return db.findMany("""
-                SELECT continents.name, countries.name, min_area 
-                FROM (SELECT continents.name, MIN(countries.area) AS min_area
+                SELECT continent_name, countries.name, min_area 
+                FROM ( SELECT continents.name AS continent_name, MIN(countries.area) AS min_area
                     FROM countries JOIN continents ON continents.id = countries.continent_id
                     GROUP BY continents.name, countries.name
                 ) AS subquery
                 LEFT JOIN countries ON countries.area = subquery.min_area
                 """, ResultSetMappers.STR_STR_INT_MAPPER);
     }
+
+    // 5. Find all continents, which have total area greater than 15 million km2 (names)
+    public List<String> continentsWithAreaGreaterThan(int area) throws SQLException {
+        return db.findMany("""
+                SELECT continents.name
+                FROM continents JOIN countries ON continents.id = countries.continent_id
+                GROUP BY continents.name
+                HAVING SUM(countries.area) > ?
+                """, ResultSetMappers.STRING_MAPPER, area);
+    }
+
+    // 6. Find all continents, which have average country population less than 20 million (names)
+    public List<String> continentsWithAverageCountryPopulationLessThan(int population) throws SQLException {
+        return db.findMany("""
+                SELECT continents.name
+                FROM continents JOIN countries ON continents.id = countries.continent_id
+                GROUP BY continents.name
+                HAVING AVG(countries.population) < ?
+                """, ResultSetMappers.STRING_MAPPER, population);
+    }
+
 }

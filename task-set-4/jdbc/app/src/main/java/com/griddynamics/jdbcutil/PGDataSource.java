56c1bf5494349164dd8e1015f79db7bb81149d77
Hubert Mazur <hmazur@griddynamics.com>
2022-11-02 23:10:40 +0100

    Change PGDataSource to initialize datasource only once

--- task-set-4/jdbc/app/src/main/java/com/griddynamics/jdbcutil/PGDataSource.java
+++ task-set-4/jdbc/app/src/main/java/com/griddynamics/jdbcutil/PGDataSource.java
@@ -7,20 +7,19 @@ import org.postgresql.ds.PGSimpleDataSource;
 
 public class PGDataSource implements DatabaseConnectionProvider {
 
-    private final ConnectionAttributes attributes;
+    private final PGSimpleDataSource dataSource;
 
-    public PGDataSource(ConnectionAttributes connectionAttributes) {
-        this.attributes = connectionAttributes;
-    }
-
-    @Override
-    public Connection getConnection() throws SQLException {
-        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+    public PGDataSource(ConnectionAttributes attributes) {
+        this.dataSource = new PGSimpleDataSource();
         dataSource.setDatabaseName(attributes.databaseName());
         dataSource.setServerNames(new String[]{attributes.ipAddress().getHostAddress()});
         dataSource.setPortNumbers(new int[]{attributes.port()});
         dataSource.setUser(attributes.user());
         dataSource.setPassword(attributes.password());
+    }
+
+    @Override
+    public Connection getConnection() throws SQLException {
         return dataSource.getConnection();
     }
     

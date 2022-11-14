e6a70c675b5f8eeed969e886d16f042ddb956221
Hubert Mazur <hmazur@griddynamics.com>
2022-11-14 19:29:56 +0100

    Extract load factor to a static variable

--- task-set-5/lru-cache/lib/src/main/java/com/griddynamics/LRUCache.java
+++ task-set-5/lru-cache/lib/src/main/java/com/griddynamics/LRUCache.java
@@ -8,16 +8,21 @@ public class LRUCache<K, V> extends Cache<K, V> {
     /*
      * This value helps preventing map from rehasing which is a
      * costly and unnecesary action which must be avoided in cache
-     * implementations. Default load factor of the HashMap is 0.75.
+     * implementations.
      */
     private static final int MAP_CAPPACITY_MULT = 2;
+
+    /*
+     * Default load factor of the HashMap is 0.75.
+     */
+    private static final float MAP_LOAD_FACTOR = 0.75f;
 
     /**
      * Creates a {@code Cache} implementing LRU eviction strategy using {@code LinkedHashMap}
      * @param capacity defines maximum number of cached entries
      */
     public LRUCache(int capacity) {
-        super(capacity, new LinkedHashMap<K, CachedValue<V>>(MAP_CAPPACITY_MULT * capacity, .75f, true) {
+        super(capacity, new LinkedHashMap<K, CachedValue<V>>(MAP_CAPPACITY_MULT * capacity, MAP_LOAD_FACTOR, true) {
             
             /*
             * The method:
